9731b01af78e6b11d1e4f97c3714a0ec8d3a00a3
Mark Del Rosario <MarkESDR@gmail.com>
2018-12-04 20:37:38 -0500

    Add even more programs

--- antergos/setup.sh
+++ antergos/setup.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 yay -S \
+  # Dev Env
+  python \
+  postgresql \
+  redis \
+  watchman \
+  inotify-tools \
+  yarn \
   # Dev Tools
   vim \
+  neovim \
+  python-neovim \
+  xclip \
   postman-bin \
   roxterm \
-  # Dev Env
-  postgresql \
-  redis
-  # Desktop
+  tmux \
+  # i3
   i3lock-color-git \
-  i3lock-fancy
+  i3lock-fancy \
+  # Applications
+  thunderbird
